150d9c8544073d3aff59612fc0362fa1680d43b7
Burak Yauhen <burakyauhen@gmail.com>
2023-02-11 13:20:20 +0300

    feat: add algorithm

--- src/index.js
+++ src/index.js
@@ -1,6 +1,24 @@
-
-// You should implement your task here.
-
 module.exports = function towelSort (matrix) {
-  return [];
+  let arr = [];
+
+    function direct(i) {
+        for (let j = 0; j < matrix[i].length; j++){
+            arr.push(matrix[i][j]);
+        }
+    }
+
+    function reverse(i) {
+        for (let j = matrix[i].length -1; j >= 0; j--){
+            arr.push(matrix[i][j]);
+        }
+    }
+
+    if (!matrix) return arr;
+
+    for (let i = 0; i < matrix.length; i++) {
+        direct(i);
+        i++;
+        if (i < matrix.length) reverse(i);
+    }
+    return arr;
 }
